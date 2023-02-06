40dcf93fe3fb364d7c11a259f8a7c2fa3e63778e
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-06 00:54:03 -0500

    Add error-handling middleware for common errors and debugging

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -15,6 +15,7 @@ const app = express();
 
 //Adding routes to the Express application
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 //Connect the morgan middleware for logging information about requests and responses:
 app.use(morgan('dev'));
@@ -59,6 +60,46 @@ app.use(
 
 //Routes
 app.use(routes); // Connect all the routes
+
+//Error handling middleware: https://expressjs.com/en/guide/using-middleware.html#middleware.error-handling
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+    //If this resource-not-found middleware is called, an error will be created with the message "The requested resource couldn't be found." and a status code of 404. Afterwards, next will be invoked with the error. Remember, next invoked with nothing means that error handlers defined after this middleware will not be invoked. However, next invoked with an error means that error handlers defined after this middleware will be invoked.
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+    //If the error that caused this error-handler to be called is an instance of ValidationError from the sequelize package, then the error was created from a Sequelize database validation error and the additional keys of title string and errors array will be added to the error and passed into the next error handling middleware.
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+
+    //The last error handler is for formatting all the errors before returning a JSON response. It will include the error message, the errors array, and the error stack trace (if the environment is in development) with the status code of the error message.
+
 
 //Export the app
 module.exports = app;
