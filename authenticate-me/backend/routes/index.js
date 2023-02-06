b11ff597bd64bf14e6f366bbc60d8e4c15a8c475
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-06 00:36:31 -0500

    Initialize Express and Sequelize with CSRF protection

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -4,10 +4,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+//This is the first test we used to see if our set up was functioning properly:
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+//Add a route, GET /api/csrf/restore to allow any developer to re-set the CSRF token cookie XSRF-TOKEN.
+    // In this route, you are setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return. Then, send the token as the response for easy retrieval.
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
 
 module.exports = router;
 
