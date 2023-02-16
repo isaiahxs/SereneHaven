8cc3804ebec8076851e9fe5cd95787aedef56a0f
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-16 18:07:41 -0500

    updated backend/utils/validation.js

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,3 +1,30 @@
+//This was the old way, before Blake gave us the updated code
+// // backend/utils/validation.js
+// const { validationResult } = require('express-validator');
+
+// // middleware for formatting errors from express-validator middleware
+// // (to customize, see express-validator's documentation)
+// const handleValidationErrors = (req, _res, next) => {
+//   const validationErrors = validationResult(req);
+
+//   if (!validationErrors.isEmpty()) {
+//     const errors = validationErrors
+//       .array()
+//       .map((error) => `${error.msg}`);
+
+//     const err = Error('Bad request.');
+//     err.errors = errors;
+//     err.status = 400;
+//     err.title = 'Bad request.';
+//     next(err);
+//   }
+//   next();
+// };
+
+// module.exports = {
+//   handleValidationErrors
+// };
+
 // backend/utils/validation.js
 const { validationResult } = require('express-validator');
 
@@ -7,14 +34,15 @@ const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = validationErrors
+    const errors = {};
+    validationErrors
       .array()
-      .map((error) => `${error.msg}`);
+      .forEach(error => errors[error.param] = error.msg);
 
-    const err = Error('Bad request.');
+    const err = Error("Bad request.");
     err.errors = errors;
     err.status = 400;
-    err.title = 'Bad request.';
+    err.title = "Bad request.";
     next(err);
   }
   next();
