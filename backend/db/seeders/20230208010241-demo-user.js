bcc4a0587caa14301774b599b3b62baa9ce5b4ff
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-15 20:53:42 -0500

    edited user migration to have the type thing above options

--- backend/db/seeders/20230208010241-demo-user.js
+++ backend/db/seeders/20230208010241-demo-user.js
@@ -1,12 +1,13 @@
 'use strict';
 const bcrypt = require('bcryptjs');
 
+/** @type {import('sequelize-cli').Migration} */
+
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
