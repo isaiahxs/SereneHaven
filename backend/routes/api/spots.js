c9859576e9cb46d2d3cf9da2b35738fd43e9e94c
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-23 01:41:28 -0500

    completed get all spots owned by current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -77,8 +77,7 @@ router.get('/', async (req, res, next) => {
 })
 
 //Create a spot
-//I NEED TO FIND OUT WHY WHEN I PUT IN AN EMPTY STRING FOR ADDRESS AND NAME IT SAYS Invalid value INSTEAD OF "Street address is required"
-    //NEED TO FIX PRICE AS WELL, it accepts strings at the moment
+//Price accepts a string at the moment
     //REQUIRE AUTH MUST BE TRUE
 router.post('/', requireAuth, validateSpot, async (req, res, next) => {
     //if user is authenticated,
@@ -127,7 +126,65 @@ router.post('/', requireAuth, validateSpot, async (req, res, next) => {
     }
 })
 
+//Get all spots owned by the current user
+    //REQUIRE AUTH: TRUE
+    //NEED TO GET 'Spots' ARRAY TO SHOW
+router.get('/current', requireAuth, async (req, res, next) => {
+    //if user is authenticated, findAll spots that belong to user from Spot model
+    const usersSpots = await Spot.findAll({
+        //filter spots by authenticated user's id
+        where: {ownerId: req.user.id},
+        attributes: [
+            //specify which columns to include in the result set
+            'id',
+            'ownerId',
+            'address',
+            'city',
+            'state',
+            'country',
+            'lat',
+            'lng',
+            'name',
+            'description',
+            'price',
+            'createdAt',
+            'updatedAt',
+            [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgRating'],
+            [Sequelize.fn('MAX', Sequelize.col('SpotImages.url')), 'previewImage']
+        ],
+        //specify models to eager load, only including respective foreign keys
+            //this way we can use the JOIN to fetch reviews and images for each spot without having to include full details
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+            {
+                model: SpotImage,
+                attributes: []
+            }
+        ],
+        group: ['Spot.id', 'SpotImages.id', 'Reviews.spotId']
+    })
+    //if there were spots, return usersSpots with 200 code
+    if (usersSpots) {
+        //easy way to get array of spots as part of JSON response is to wrap usersSpots array in an object with the key of Spots
+        return res.status(200).json({Spots: usersSpots});
+    }
+    //NO SPECIFICS ON WHAT ERROR THEY WANT RETURNED
+    res.status(404).json({'message': 'This user did not have any spots'})
+})
+
+
+
+
+
 //Get details of a spot by spotId
 
+//Add image to a spot based on Spot's id
+
+//Edit a spot
+
+//Delete a spot
 
 module.exports = router;
