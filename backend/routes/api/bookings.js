f93984ca389fd6b0d5280f6950e7e0bb4fd687c6
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-23 21:26:12 -0500

    completed success GET current user's bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -23,14 +23,125 @@ const validateBookings = [
     handleValidationErrors
 ]
 
-//Get all of the current user's bookings FIRST NEED TO CREATE BOOKING (IN SPOTS.JS) OR ELSE I'LL GET SOMETHING EMPTY HERE
-//     //REQUIRE AUTH: TRUE
+//FIRST WIP
+//Get all of the current user's bookings
+    //REQUIRE AUTH: TRUE
 // router.get('/current', requireAuth, async (req, res, next) => {
-
-
 //     //if current user is authorized, return status 200 with json of bookings
+//     //retrieve current user
+//     const currentUser = req.user.id;
+
+//     const userBookings = await Booking.findAll({
+//         where: {
+//             userId: currentUser
+//         },
+//         attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt']
+//     })
+
+//     for (let booking of userBookings) {
+//         const spot = await Spot.findOne({
+//             where: {
+//                 ownerId: currentUser
+//             },
+//             attributes: [
+//                 'id',
+//                 'ownerId',
+//                 'address',
+//                 'city',
+//                 'state',
+//                 'country',
+//                 'lat',
+//                 'lng',
+//                 'name',
+//                 'price'
+//                 //HOW CAN I RETURN PREVIEWIMAGE HERE
+//                 // [Sequelize.fn('MAX', Sequelize.col('SpotImages.url')), 'previewImage']
+//             ],
+//             //SOMETHING LIKE THIS, LOOK AT HOW I DID IT IN REVIEWS.JS
+//             // include: [
+//             //     {
+//             //         model: SpotImage,
+//             //         attributes: ['url']
+//             //     }
+//             // ]
+//         })
+//         booking.dataValues.Spot = spot
+//     }
+//     return res.status(200).json({Bookings: userBookings});
 // })
 
+//SECOND WIP FOR GET CURRENT USER'S BOOKINGS
+    //FOLLOWED SIMILAR STYLE AS REVIEWS.JS AND IT WORKS!
+router.get('/current', requireAuth, async (req, res, next) => {
+    const currentUser = req.user.id;
+    const userBookings = await Booking.findAll({
+      where: {
+        userId: currentUser,
+      },
+      include: [
+        {
+          model: Spot,
+          attributes: [
+            'id',
+            'ownerId',
+            'address',
+            'city',
+            'state',
+            'country',
+            'lat',
+            'lng',
+            'name',
+            'price',
+          ],
+          include: [
+            {
+              model: SpotImage,
+              attributes: ['url'],
+              limit: 1,
+            },
+          ],
+        },
+      ],
+      attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt'],
+    });
+
+    //TESTING SIMILAR STYLE TO REVIEWS.JS
+    if (userBookings) {
+        const bookings = userBookings.map((booking) => {
+            //convert each booking model into plain JS object with toJSON
+            //destructure and extract only necessary properties
+            const {id, spotId, userId, startDate, endDate, createdAt, updatedAt, Spot} = booking.toJSON();
+
+            let previewImage = null;
+            if (Spot.SpotImages.length > 0) {
+                previewImage = Spot.SpotImages[0].url;
+            } else {
+                previewImage = null;
+            }
+
+            //return the order you want the response body in
+            return {
+                id, spotId, userId, startDate, endDate, createdAt, updatedAt, Spot: {
+                    id: Spot.id,
+                    ownerId: Spot.ownerId,
+                    address: Spot.address,
+                    city: Spot.city,
+                    state: Spot.state,
+                    country: Spot.country,
+                    lat: Spot.lat,
+                    lng: Spot.lng,
+                    name: Spot.name,
+                    price: Spot.price,
+                    previewImage
+                }
+            }
+        })
+        return res.status(200).json({ Bookings: bookings });
+    }
+    return res.status(404).json({
+        message: "Reviews couldn't be found"
+    })
+  });
 
 
 
