8fd80b37a9d57ea4e6ca25b89720dcc69b0d3c03
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-25 14:32:10 -0500

    completed get all bookings by spot id and edited Booking(s) model and migration file data types for startDate endDate

--- backend/db/migrations/20230214233730-create-booking.js
+++ backend/db/migrations/20230214233730-create-booking.js
@@ -28,11 +28,11 @@ module.exports = {
         //should i have alloNull false here?
       },
       startDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         allowNull: false
       },
       endDate: {
-        type: Sequelize.DATE,
+        type: Sequelize.DATEONLY,
         allowNull: false
       },
       createdAt: {
