85bf8a3597bba58586b136a450fc8f4104e0032b
isaiahxs <isaiahsinnathamby@gmail.com>
2023-02-27 03:46:34 -0500

    changed authenticate message to be

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -63,8 +63,8 @@ const restoreUser = (req, res, next) => {
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
 
-    const err = new Error('Unauthorized');
-    err.title = 'Unauthorized';
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
     // err.errors = ['Unauthorized'];
     err.status = 401;
     return next(err);
