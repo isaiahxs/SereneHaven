fa2dc61ae9d9373ebdc3e86004503ad2a2417dd7
isaiahxs <isaiahsinnathamby@gmail.com>
2023-04-03 18:53:52 -0400

    worked on first half of fep4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
+
 import './index.css';
+
+import ReactDOM from 'react-dom';
+// import { Provider as ReduxProvider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider, Modal } from './context/Modal';
 import App from './App';
+
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 //import all the actions from session.js
@@ -20,13 +25,16 @@ if (process.env.NODE_ENV !== "production") {
   window.sessionActions = sessionActions;
 }
 
+//wrap the application with the Modal provider and render the modal component after the App component so that all the Modal content will be layered as HTML elements on top of all the other HTML elements
 function Root() {
   return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
