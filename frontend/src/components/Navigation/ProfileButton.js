3c0db50c0ebd0cb8ebc7d28d60d057c5c956e3d9
isaiahxs <isaiahsinnathamby@gmail.com>
2023-04-03 20:45:01 -0400

    added bonus including the different optional dropdowns

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,66 +1,9 @@
-//my original profile button
-// import React, {useState, useEffect, useRef} from "react";
-// import { useDispatch } from "react-redux";
-// import * as sessionActions from '../../store/session';
-
-// //create a React functional component called ProfileButton that will render a generic user profile icon of your choice from Font Awesome
-// function ProfileButton({user}) {
-//     const dispatch = useDispatch();
-//     const [showMenu, setShowMenu] = useState(false);
-//     const ulRef = useRef();
-
-//     const openMenu = () => {
-//         if(showMenu) return;
-//         setShowMenu(true);
-//     }
-
-//     useEffect(() => {
-//         if (!showMenu) return;
-//         const closeMenu = (e) => {
-//             if (!ulRef.current.contains(e.target)) {
-//                 setShowMenu(false);
-//             }
-//         }
-
-//         document.addEventListener('click', closeMenu)
-
-//         return () => document.removeEventListener('click', closeMenu);
-//     }, [showMenu])
-
-//     const logout = (e) => {
-//         e.preventDefault();
-//         dispatch(sessionActions.logout());
-//     }
-
-//     const ulClassName = 'profile-dropdown' + (showMenu ? "" : " hidden");
-
-//     return (
-//         <>
-//             <button onClick={openMenu}>
-//                 <i className='fas fa-user-circle' />
-//                 {/* <i clasName='fa-solid fa-user'></i> */}
-//             </button>
-//             <ul className={ulClassName} ref={ulRef}>
-//                 <li>{user.username}</li>
-//                 <li>{user.firstName} {user.lastName}</li>
-//                 <li>{user.email}</li>
-//                 <li>
-//                     <button onClick={logout}>Log Out</button>
-//                 </li>
-//             </ul>
-//         </>
-//     )
-// }
-
-// export default ProfileButton;
-
-//--------------------------------------------------------
-
-//their example profile button
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import OpenModalButton from "../OpenModalButton";
+//below is if i want log in and sign up to not appear as buttons
+// import OpenModalMenuItem from "./OpenModalMenuItem";
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from "../SignupFormModal";
 
@@ -88,9 +31,12 @@ function ProfileButton({ user }) {
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
+  const closeMenu = () => setShowMenu(false);
+
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    closeMenu();
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -115,12 +61,18 @@ function ProfileButton({ user }) {
             <li>
               <OpenModalButton
                 buttonText="Log In"
+              // <OpenModalMenuItem
+              //   itemText="Log In"
+                onButtonClick={closeMenu}
                 modalComponent={<LoginFormModal />}
               />
             </li>
             <li>
               <OpenModalButton
                 buttonText="Sign Up"
+              // <OpenModalMenuItem
+              //   itemText="Sign Up"
+                onButtonClick={closeMenu}
                 modalComponent={<SignupFormModal />}
               />
             </li>
