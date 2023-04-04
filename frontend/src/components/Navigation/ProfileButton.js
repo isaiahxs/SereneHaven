bc51ea8c1045f08cc1e039a4001b9fd035b34c6f
isaiahxs <isaiahsinnathamby@gmail.com>
2023-04-03 20:31:23 -0400

    completed profile button part of bonus modal

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -60,6 +60,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from "../SignupFormModal";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -98,12 +101,31 @@ function ProfileButton({ user }) {
         <i className="fas fa-user-circle" />
       </button>
       <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
       </ul>
     </>
   );
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,91 +1,22 @@
-//my original code
-// import React from "react";
-// import { NavLink } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import ProfileButton from './ProfileButton';
-// import './Navigation.css';
-
-// //add a React functional component named Navigation
-// function Navigation({isLoaded}) {
-//     const sessionUser = useSelector(state => state.session.user);
-
-//     let sessionLinks;
-//     if (sessionUser) {
-//         //should contain a logout button when there is a sessionUser
-//         sessionLinks = (
-//             <li>
-//                 <ProfileButton user={sessionUser}/>
-//             </li>
-//         )
-//     } else {
-//         //it should only contain navigation links to the login and signup routes when there is no session user
-//         sessionLinks = (
-//             //BULLET POINTS BECAUSE THESE ARE LI TAGS
-//             <li>
-//                 <NavLink to='/login'>Log In</NavLink>
-//                 <br></br>
-//                 <NavLink to='/signup'>Sign Up</NavLink>
-//             </li>
-//         )
-//     }
-
-//     return (
-//         //should render an unordered list with a navigation link to the home page
-//         <ul>
-//             <li>
-//                 <NavLink exact to='/'>Home</NavLink>
-//             </li>
-//             {isLoaded && sessionLinks}
-//         </ul>
-//     )
-// }
-
-// export default Navigation;
-
-//--------------------------------------------------------
-
-//their example code
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-      <li>
-        <OpenModalButton
-        buttonText="Log In"
-        modalComponent={<LoginFormModal/>}
-        />
-
-        <OpenModalButton
-        buttonText="Sign Up"
-        modalComponent={<SignupFormModal/>}
-        />
-      </li>
-    );
-  }
 
   return (
     <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
