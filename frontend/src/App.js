727d66add97b96e6174286d4984f4996288954d0
isaiahxs <isaiahsinnathamby@gmail.com>
2023-04-03 20:23:49 -0400

    converted signup form to modal - finished phase 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from "react";
 import { useDispatch } from "react-redux";
-import {Route, Switch} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
 //in phase 4, we reconfigure LoginFormPage to be a modal instead of a page
 // import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from "./components/SignupFormPage";
+//bonus refactor
+// import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from './store/session';
 import Navigation from "./components/Navigation";
 
@@ -24,9 +25,9 @@ function App() {
           {/* <Route path="/login">
             <LoginFormPage />
           </Route> */}
-          <Route path="/signup">
+          {/* <Route path="/signup">
             <SignupFormPage />
-          </Route>
+          </Route> */}
         </Switch>
       )}
     </>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -51,6 +51,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -70,7 +71,11 @@ function Navigation({ isLoaded }){
         buttonText="Log In"
         modalComponent={<LoginFormModal/>}
         />
-        <NavLink to="/signup">Sign Up</NavLink>
+
+        <OpenModalButton
+        buttonText="Sign Up"
+        modalComponent={<SignupFormModal/>}
+        />
       </li>
     );
   }
