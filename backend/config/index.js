61478ab0770dfb6a15be1a416608590c5bebe630
isaiah <isaiahsinnathamby@gmail.com>
2023-06-12 11:18:24 -0400

    modified use_env_variable to use new database instance internal db url

--- backend/config/index.js
+++ backend/config/index.js
@@ -10,7 +10,8 @@ module.exports = {
 
     //adding production key including internal database url for Postgres (this is from troubleshooting project configuration)
     production: {
-      use_env_variable: 'postgres://app_academy_projects_2w5m_user:r6edBUHr4fyzuvmfhxWUMy8TFdcARaHp@dpg-cfjsu7hmbjsn9e9keip0-a/app_academy_projects_2w5m',
+      // use_env_variable: 'postgres://app_academy_projects_2w5m_user:r6edBUHr4fyzuvmfhxWUMy8TFdcARaHp@dpg-cfjsu7hmbjsn9e9keip0-a/app_academy_projects_2w5m',
+      use_env_variable: 'postgres://aa_projects_big5_user:KZT4l1v3iucepsa2pQuT9R4o6E4JPTth@dpg-chf8ejgrddl9buirjle0-a/aa_projects_big5',
       dialect: 'postgres',
       // ...
     }
