4ef18f4d8d59436a78d052a5ef733c780f6e359c
isaiah <isaiahsinnathamby@gmail.com>
2023-08-08 19:53:20 -0400

    can view favorites in ManageFavorites

--- frontend/src/components/ManageFavorites/index.js
+++ frontend/src/components/ManageFavorites/index.js
@@ -8,15 +8,65 @@ import { getFavorites } from '../../store/favorites';
 
 function ManageFavorites() {
     const dispatch = useDispatch();
+    const history = useHistory();
+
+    const userFavorites = useSelector((state) => state.favorites);
+    // console.log(userFavorites);
 
     useEffect(() => {
         dispatch(getFavorites())
-    })
-    return (
-        <div className='manage-favorites-container'>
-            <h2>Manage Favorites</h2>
-        </div>
-    )
+    }, []);
+
+    const spotDetails = (e, id) => {
+        e.preventDefault();
+        history.push(`/spots/${id}`)
+    }
+
+    if (userFavorites) {
+        return (
+            <>
+                <div id='manage-header' className="manage-header">
+                    <h1>Manage Favorites</h1>
+                </div>
+
+                <div className='no-current-spots'>
+                    {userFavorites?.length === 0 &&
+                        <h2>Looks like you don't have any favorites yet! Once you have some, you can view them here.</h2>
+                    }
+                </div>
+
+                <div className='landing-container'>
+                    {userFavorites?.length > 0 &&
+                        <>
+                            {userFavorites.map((favorite) => (
+                                <div key={favorite.id} className='spot-card'>
+                                    <>
+                                        <div onClick={(e) => { spotDetails(e, favorite.id) }}>
+                                            <div className='spot-card-img'>
+                                                <img className='spot-image' src={favorite?.previewImage} alt={favorite?.name} />
+                                            </div>
+                                            <div className='manage-card-container'>
+                                                <div className='manage-spot-info'>
+                                                    {favorite?.city}, {favorite?.state}
+                                                </div>
+                                                <div className='manage-price'>
+                                                    <span className='location-price'>${favorite?.price}</span> night
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>
+                                </div>
+                            ))}
+                        </>
+                    }
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <div className='loading'>Loading...</div>
+        )
+    }
 }
 
 export default ManageFavorites;
